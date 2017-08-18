208102434bf627f1327d9004643cfa22cd90afc6
ebinezerp <ebinezer.p87@gmail.com>
2017-08-18 18:55:03 +0530

    backend attached

---
+++ backendmodule/src/main/java/com/wave2test/backendmodule/App.java
@@ -0,0 +1,13 @@
+package com.wave2test.backendmodule;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
