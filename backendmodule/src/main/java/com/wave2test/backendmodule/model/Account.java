8ab80e7854fbb0140aa15d12365903853209cbd3
ebinezerp <ebinezer.p87@gmail.com>
2017-08-20 10:02:15 +0530

    done

--- backendmodule/src/main/java/com/wave2test/backendmodule/model/Account.java
+++ backendmodule/src/main/java/com/wave2test/backendmodule/model/Account.java
@@ -8,6 +8,10 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
 @Table(uniqueConstraints=@UniqueConstraint(columnNames={"userName"}))
@@ -16,12 +20,30 @@ public class Account {
 	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE)
 	private Long accountId;
+	@NotNull
+	@NotBlank
+	@Pattern(regexp="[a-z]{2,10}",message="only small alphabet, Min 2 and Max 10 characters")
 	private String firstName;
+	@NotNull
+	@NotBlank
+	@Pattern(regexp="[a-z]{5,10}",message="only small alphabet, Min 5 and Max 10 characters")
 	private String lastName;
+	@NotNull
+	@NotBlank
+	@Pattern(regexp="^[_a-z0-9-]+(\\.[_a-z0-9-]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)*(\\.[a-z]{2,4})$",message="enter a valid email")
 	private String email;
+	@NotNull
+	@NotBlank
+	@Pattern(regexp="^[789]\\d{9}$",message="enter a valid Mobile Number")
 	private String mobile;
+	@NotNull
+	@NotBlank
+	@Pattern(regexp="[a-z]{8,}",message="small alphabet, Min 8 characters")
 	@Column(unique = true)
 	private String userName;
+	@NotNull
+	@NotBlank
+	@Pattern(regexp="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,10}",message="Min 8 characters Max 10 characters,Atleast one small, one capital alphabet,one digit and one special character")
 	private String password;
 	@Transient
 	private String cpassword;
--- restservices/src/main/java/com/wave2test/restservices/controller/UserController.java
+++ restservices/src/main/java/com/wave2test/restservices/controller/UserController.java
@@ -1,12 +1,26 @@
 package com.wave2test.restservices.controller;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wave2test.backendmodule.dao.AccountDao;
@@ -19,21 +33,44 @@ public class UserController {
 	@Autowired
 	AccountDao accountDao;
 	
+	@Autowired
+	HttpSession session;
+	
 	@PostMapping("/register")
-	public ResponseEntity<Boolean> register(@RequestBody Account account)
+	public ResponseEntity<Map<String,String>> register(@RequestBody @Valid Account account,BindingResult result,HttpServletResponse response,HttpServletRequest request)
 	{
+		Map<String,String> errorMap=new HashMap<String,String>();
+		System.out.println("result of the account object::::"+result.hasErrors());
+		if(result.hasErrors())
+		{
+			System.out.println("error occured");
+			
+			
+		for(FieldError error:result.getFieldErrors())
+		{
+		  
+		  errorMap.put(error.getField(),error.getDefaultMessage());
+		}
+		
+		     
+		 
+		     return new ResponseEntity<Map<String,String>>(errorMap,HttpStatus.BAD_REQUEST);
+		}
+		else{
 		Boolean b=accountDao.createAccount(account);
+		
 		if(b)
 		{
-			return new ResponseEntity<Boolean>(b,HttpStatus.CREATED);
+			return new ResponseEntity<Map<String,String>>(errorMap,HttpStatus.CREATED);
 		}else
 		{
-			return new ResponseEntity<Boolean>(b,HttpStatus.BAD_REQUEST);
+			 return new ResponseEntity<Map<String,String>>(errorMap,HttpStatus.BAD_REQUEST);
+		}
 		}
 	}
 	
 	@PostMapping("/login")
-	public ResponseEntity<Account> login(@RequestBody Account account,HttpSession session)
+	public ResponseEntity<Account> login(@RequestBody Account account)
 	{
 		Account act= accountDao.getUserByUserName(account.getUserName());
 		if(act!=null)
@@ -45,6 +82,35 @@ public class UserController {
 		{
 			return new ResponseEntity<Account>(act,HttpStatus.UNAUTHORIZED);
 		}
+	}
+	
+	/*@PostMapping("/error")
+	public ResponseEntity<Map<String,String>> errorMessages(@RequestAttribute("errorMap") Object errorMap)
+	{
+		
+		
+		Map<String, String> errorMapm=(Map<String,String>)errorMap;
+		for(String s:errorMapm.values())
+		{
+			System.out.println(s);
+		}
+		return new ResponseEntity<Map<String,String>>(errorMapm,HttpStatus.BAD_REQUEST);
+	}*/
+	
+	@GetMapping("/logout")
+	public ResponseEntity<Boolean> logout()
+	{
+		try{
+		session.removeAttribute("username");
+		session.invalidate();
+		return new ResponseEntity<Boolean>(true,HttpStatus.FORBIDDEN);
+		}catch (Exception e) {
+			// TODO: handle exception
+			
+			System.out.println(e);
+			return new ResponseEntity<Boolean>(false,HttpStatus.EXPECTATION_FAILED);
+		}
+		
 	}
 
 }
